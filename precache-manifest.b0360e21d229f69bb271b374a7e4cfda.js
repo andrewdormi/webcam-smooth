98f5533564524cdcabb56d836d1fae03b999aad1
andrey.d <andrey.d@fora-soft.com>
2020-05-21 09:56:02 +0300

    Updates

---
+++ precache-manifest.b0360e21d229f69bb271b374a7e4cfda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299a1a5c6d158965c189b7ebe3d737b5",
+    "url": "/webcam-smooth/index.html"
+  },
+  {
+    "revision": "b42a81369a717a6d4a88",
+    "url": "/webcam-smooth/static/css/main.a08d0468.chunk.css"
+  },
+  {
+    "revision": "116910751068e7b29471",
+    "url": "/webcam-smooth/static/js/2.5e7e0612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webcam-smooth/static/js/2.5e7e0612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42a81369a717a6d4a88",
+    "url": "/webcam-smooth/static/js/main.19a0f5e1.chunk.js"
+  },
+  {
+    "revision": "a031a8e06e055e67d2e5",
+    "url": "/webcam-smooth/static/js/runtime-main.611f6a9d.js"
+  }
+]);
